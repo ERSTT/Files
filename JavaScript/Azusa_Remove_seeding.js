60e45aaf2feb5fa863e032fe0d0d7d6fcbd5d42c
ERST <158469810+ERSTT@users.noreply.github.com>
2024-02-17 17:04:59 +0800

    Create Azusa_Remove_seeding.js

---
+++ JavaScript/Azusa_Remove_seeding.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Azusa 种子页删除已做种条例
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Azusa查找未做种带自动翻页
+// @author       ERST
+// @match        https://azusa.wiki/torrents*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 删除已做种条例
+    let divElements = document.querySelectorAll('div[title="seeding 100%"]');
+    divElements.forEach(divElement => {
+        let trElement = divElement.closest('tr');
+        if (trElement) {
+            let parentTrElement = trElement.parentNode.closest('tr');
+            if (parentTrElement) {
+                parentTrElement.remove();
+            }
+            trElement.remove();
+        }
+    });
+})();
