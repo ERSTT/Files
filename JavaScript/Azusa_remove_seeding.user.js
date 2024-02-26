c62636bf756ff4e9c6302e0d4f29d6a14211bc6a
ERST <158469810+ERSTT@users.noreply.github.com>
2024-02-27 02:06:04 +0800

    Update Azusa_remove_seeding.user.js

--- JavaScript/Azusa_remove_seeding.user.js
+++ JavaScript/Azusa_remove_seeding.user.js
@@ -2,11 +2,13 @@
 // @name         Azusa 种子页删除已做种种子
 // @namespace    https://github.com/ERSTT
 // @icon         https://azusa.wiki/favicon.ico
-// @version      0.1
+// @version      0.2
 // @description  Azusa 种子页删除已做种种子
 // @author       ERST
 // @match        https://azusa.wiki/*torrents*
 // @grant        none
+// @updateURL    https://github.com/ERSTT/Files/raw/main/JavaScript/Azusa_remove_seeding.user.js
+// @downloadURL  https://github.com/ERSTT/Files/raw/main/JavaScript/Azusa_remove_seeding.user.js
 // ==/UserScript==
 
 (function() {
