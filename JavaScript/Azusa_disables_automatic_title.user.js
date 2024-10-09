ffb8ab06ef4853459f4d39f686b01875ed690955
ERST <158469810+ERSTT@users.noreply.github.com>
2024-10-09 15:49:45 +0800

    Update Azusa_disables_automatic_title.user.js

--- JavaScript/Azusa_disables_automatic_title.user.js
+++ JavaScript/Azusa_disables_automatic_title.user.js
@@ -2,13 +2,15 @@
 // @name         Azusa 禁用发布页上传种子文件自动填写标题
 // @namespace    https://github.com/ERSTT
 // @icon         https://azusa.wiki/favicon.ico
-// @version      0.1
+// @version      1.0
 // @description  Azusa 禁用发布页上传种子文件自动填写标题
 // @author       ERST
 // @match        https://azusa.wiki/*upload*
+// @match        https://zimiao.icu/*upload*
 // @grant        none
-// @updateURL    https://github.com/ERSTT/Files/raw/main/JavaScript/Azusa_disables_automatic_title.user.js
-// @downloadURL  https://github.com/ERSTT/Files/raw/main/JavaScript/Azusa_disables_automatic_title.user.js
+// @updateURL    https://raw.githubusercontent.com/ERSTT/Files/refs/heads/main/JavaScript/Azusa_disables_automatic_title.user.js
+// @downloadURL  https://raw.githubusercontent.com/ERSTT/Files/refs/heads/main/JavaScript/Azusa_disables_automatic_title.user.js
+// @changelog    添加新域名
 // ==/UserScript==
 
 (function() {
