ae8ac55060f37410f395fc74adfd7f55600f1f69
ERST <158469810+ERSTT@users.noreply.github.com>
2025-01-18 02:05:52 +0800

    Update Azusa_card_marking_not_owned.user.js

--- JavaScript/Azusa_card_marking_not_owned.user.js
+++ JavaScript/Azusa_card_marking_not_owned.user.js
@@ -16,14 +16,10 @@
 (function() {
     'use strict';
 
-    // 获取当前页面的域名
     var currentDomain = window.location.hostname;
-
-    // 获取URL中的csrf_token参数
     var urlParams = new URLSearchParams(window.location.search);
     var csrfToken = urlParams.get('csrf_token');
 
-    // 动态构建 URL
     var url1 = `https://${currentDomain}/lotterySettingSave.php?csrf_token=${csrfToken}&action=userCharacterCards`;
     var url2 = `https://${currentDomain}/lotterySettingSave.php?csrf_token=${csrfToken}&action=specialExchangeCharacterCardsPool`;
     var url3 = `https://${currentDomain}/lotterySettingSave.php?csrf_token=${csrfToken}&action=exchangeCharacterCardsPool`;
@@ -32,7 +28,6 @@
     var combinedResult = [];
     var ownedCardIds = [];
 
-    // 初次获取数据并进行对比
     function fetchDataAndMark() {
         GM_xmlhttpRequest({
             method: "GET",
@@ -45,16 +40,14 @@
                     .then(results => {
                         combinedResult = results.flat();
 
-                        // 定期渲染标记
                         setInterval(() => {
-                            markCards(combinedResult, "red");  // 只使用缓存数据
+                            markCards(combinedResult, "red");
                         }, 500);
                     });
             }
         });
     }
 
-    // 获取其他卡片数据并排除已拥有的卡片
     function fetchCards(url, excludeIds) {
         return new Promise(resolve => {
             GM_xmlhttpRequest({
@@ -72,23 +65,19 @@
         });
     }
 
-    // 标记卡片
     function markCards(cards) {
         cards.forEach(function(item) {
             var elements = document.querySelectorAll('img[src="' + item.pic + '"]');
             elements.forEach(function(element) {
-                var elImage = element.closest('.el-image'); // 获取 el-image 容器
+                var elImage = element.closest('.el-image');
 
                 if (elImage) {
-                    // 检查是否已经有标记的 span，避免重复添加
                     if (!elImage.parentNode.querySelector('.remove-to-hide-tag-for-erst-script')) {
-                        // 创建新的 span 元素
                         var span = document.createElement('span');
                         span.className = "remove-to-hide-tag-for-erst-script el-tag el-tag--danger el-tag--small el-tag--light";
                         span.style.cssText = "position: absolute; top: 10px; right: 10px; z-index: 10;";
                         span.textContent = "未拥有";
 
-                        // 插入到 el-image 的父级
                         elImage.parentNode.insertBefore(span, elImage);
                     }
                 }
@@ -96,27 +85,21 @@
         });
     }
 
-    // 动态监听按钮点击事件
     document.addEventListener('click', function (event) {
         const target = event.target;
 
-        // 检查按钮的 class 或其他属性
         if (target.closest('.el-button--danger.is-circle') && !target.id) {
             refreshData();
         }
 
-        // 检查是否点击了指定的按钮
         if (target.closest('.exchange_btn .el-button') && !target.id) {
             refreshData();
         }
     });
 
-    // 刷新数据并重新标记
     function refreshData() {
-        // 重新调用 fetchDataAndMark() 更新缓存数据和重新标记
         fetchDataAndMark();
     }
 
-    // 初始化时获取数据并进行渲染
     fetchDataAndMark();
 })();
